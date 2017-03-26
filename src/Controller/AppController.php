24643dc23d1c9cd9df56c8036eae127f3b48f0c3
Andrej Griniuk <andrej.griniuk@gmail.com>
2017-03-26 22:36:01 +0800

    fix code coverage

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,9 +6,11 @@ use App\Controller\AppController as BaseController;
 /**
  * Class AppController
  *
+ * @codeCoverageIgnore
  * @package TwoFactorAuth\Controller
  * @mixin \Cake\Controller\Controller
  */
 class AppController extends BaseController
 {
+
 }
